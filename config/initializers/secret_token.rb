d581b26430879c2240efdf7d6f0a2912f0b53cbf
Datt Dongare <datt@weboniselab.com>
2013-10-18 11:07:38 +0530

    demo app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScormDemo::Application.config.secret_token = '4d0af925b8f0feec022fc3fd2396f4699b9d4012471c6b2ad5fe369ea9504f1ab143286ae3dcb02435e1aed7d8f86d6a9610627045814ff87cfb3f9c5b15a0dd'
